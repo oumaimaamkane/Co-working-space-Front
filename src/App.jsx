fa1d8b98dc9165d33929e9bb989c80c579703c3e
codeOplomo <anasalk@gmail.com>
2024-05-17 08:32:26 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
+import React from "react";
+import MainLayout from "./components/Layouts/MainLayout";
+
+function App() {
+  return (
+    <div>
+      <MainLayout />
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/MainBanner.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+function MainBanner() {
+    
+    return (
+        <div className="home-area home-slides owl-carousel owl-theme">
+            <div className="main-banner item-bg1">
+                <div className="d-table">
+                    <div className="d-table-cell">
+                        <div className="container">
+                            <div className="main-banner-content">
+                                <span className="sub-title">The Best Workspace in New York</span>
+                                <h1>Professional, Creative, Flexible, Scalable Workspace</h1>
+
+                                <div className="btn-box">
+                                    <a href="contact.html" className="default-btn">Book A Room <span></span></a>
+                                    <a className="optional-btn" data-ilb2-video='{"controls":"controls", "autoplay":false, "sources":[{"src":"assets/img/video.m4v", "type":"video/mp4"}]}' data-imagelightbox="video"><i className="flaticon-play-button"></i> Watch Video</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="main-banner item-bg2">
+                <div className="d-table">
+                    <div className="d-table-cell">
+                        <div className="container">
+                            <div className="main-banner-content">
+                                <span className="sub-title">The Best Workspace in New York</span>
+                                <h1>Professional, Creative, Flexible, Scalable Workspace</h1>
+
+                                <div className="btn-box">
+                                    <a href="contact.html" className="default-btn">Book A Room <span></span></a>
+                                    <a className="optional-btn" data-ilb2-video='{"controls":"controls", "autoplay":false, "sources":[{"src":"assets/img/video.m4v", "type":"video/mp4"}]}' data-imagelightbox="video"><i className="flaticon-play-button"></i> Watch Video</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="main-banner item-bg3">
+                <div className="d-table">
+                    <div className="d-table-cell">
+                        <div className="container">
+                            <div className="main-banner-content">
+                                <span className="sub-title">The Best Workspace in New York</span>
+                                <h1>Professional, Creative, Flexible, Scalable Workspace</h1>
+
+                                <div className="btn-box">
+                                    <a href="contact.html" className="default-btn">Book A Room <span></span></a>
+                                    <a className="optional-btn" data-ilb2-video='{"controls":"controls", "autoplay":false, "sources":[{"src":"assets/img/video.m4v", "type":"video/mp4"}]}' data-imagelightbox="video"><i className="flaticon-play-button"></i> Watch Video</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MainBanner;
