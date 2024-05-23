43558fc4ee2e99ee2d3e1f58d79e2b7b841abb2b
medamine <aminebenmade1999@gmail.com>
2024-05-23 17:02:57 +0100

    add dark mode for categories

--- src/components/Layouts/Dashboard/Sidebar.jsx
+++ src/components/Layouts/Dashboard/Sidebar.jsx
@@ -5,30 +5,30 @@ import { FcConferenceCall } from "react-icons/fc";
 import { HiOutlineLogout } from "react-icons/hi";
 import { DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS } from "../../../lib/consts/navigation";
 
-
-const linkClasses = 'flex items-center gap-2 px-3 py-2 hover:bg-gray-200 hover:no-underline active:bg-gray-200 rounded-md text-base transition-colors duration-200 text-gray-700 font-medium';
-const activeLinkClasses = 'text-gray-900 bg-gray-200';
+const linkClasses = 'flex items-center gap-2 px-3 py-2 rounded-md text-base transition-colors duration-200 text-gray-700 dark:text-white font-medium';
+const hoverClasses = 'hover:bg-gray-200 dark:hover:bg-neutral-600 hover:no-underline active:bg-gray-200 dark:active:bg-neutral-800';
+const activeLinkClasses = 'text-orange-500 dark:text-white bg-orange-100 dark:bg-orange-500 hover:no-underline';
 
 const Sidebar = () => {
     return (
-        <aside className="bg-white w-64 p-4 flex flex-col shadow-xl">
-            <div className="flex items-center gap-3 px-2 py-4 border-b border-gray-200">
-                <FcConferenceCall fontSize={28} />
-                <span className="text-lg font-bold text-gray-900">Co-Working</span>
+        <aside className="bg-white dark:bg-neutral-700 w-64 p-4 flex flex-col shadow-xl dark:shadow-white">
+            <div className="flex items-center gap-3 px-2 py-4 border-b border-gray-400 dark:border-neutral-500">
+                <FcConferenceCall fontSize={28} className="dark:text-red" />
+                <span className="text-lg font-bold text-gray-900 dark:text-white">Co-Working</span>
             </div>
-            <nav className="flex-1 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200">
+            <nav className="flex-1 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 dark:scrollbar-thumb-neutral-600 scrollbar-track-gray-200 dark:scrollbar-track-neutral-800">
                 <ul className="flex flex-col py-4 space-y-1">
                     {DASHBOARD_SIDEBAR_LINKS.map((item) => (
                         <SidebarLink key={item.key} item={item} />
                     ))}
                 </ul>
             </nav>
-            <div className="border-t border-gray-200 pt-4 ">
+            <div className="border-t border-gray-400 dark:border-neutral-500 pt-4">
                 <ul className="flex flex-col space-y-1">
                     {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((item) => (
                         <SidebarLink key={item.key} item={item} />
                     ))}
-                    <div className={classNames('text-red-500 cursor-pointer', linkClasses)}>
+                    <div className={classNames('text-red-500 dark:text-red-600 cursor-pointer', linkClasses, hoverClasses)}>
                         <span className="text-xl"><HiOutlineLogout /></span>
                         Logout
                     </div>
@@ -45,7 +45,13 @@ function SidebarLink({ item }) {
     const isActive = pathname === item.path;
 
     return (
-        <Link to={item.path} className={classNames(isActive ? activeLinkClasses : '', linkClasses)}>
+        <Link 
+            to={item.path} 
+            className={classNames(linkClasses, { 
+                [activeLinkClasses]: isActive, 
+                [hoverClasses]: !isActive 
+            })}
+        >
             <span className="text-xl">{item.icon}</span>
             {item.label}
         </Link>
--- src/views/BackOffice/Categories.jsx
+++ src/views/BackOffice/Categories.jsx
@@ -7,7 +7,8 @@ import axios from "axios";
 
 export default function Categories() {
   const [categories, setCategories] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [newCategoryName, setNewCategoryName] = useState("");
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
@@ -34,7 +35,7 @@ export default function Categories() {
       .then((response) => {
         setCategories([...categories, response.data]);
         setNewCategoryName("");
-        setIsModalOpen(false);
+        setIsAddModalOpen(false);
         setSuccess("Category added successfully!");
         setTimeout(() => setSuccess(""), 3000);
       })
@@ -45,57 +46,58 @@ export default function Categories() {
   };
 
   return (
-    <div className="p-6 min-h-screen">
-      <div className="flex justify-between items-center py-4 px-8 bg-white rounded-lg shadow-md mb-6">
-        <span className="text-xl font-semibold text-gray-800">
+    <div className="p-6 min-h-screen dark:bg-neutral-800">
+      <div className="flex justify-between items-center py-4 px-8 dark:bg-neutral-600 rounded-lg shadow-md mb-6">
+        <span className="text-xl font-semibold text-gray-800 dark:text-white">
           List of Categories
         </span>
         <button
           className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-lg shadow-md transition duration-300 font-medium"
-          onClick={() => setIsModalOpen(true)}
+          onClick={() => setIsAddModalOpen(true)}
         >
           Add Category
         </button>
       </div>
       {success && (
-        <div className="bg-green-100 text-green-800 p-4 rounded-lg shadow-md mb-4">
+        <div className="bg-green-100 dark:bg-green-200 text-green-800 dark:text-green-900 p-4 rounded-lg shadow-md mb-4">
           {success}
         </div>
       )}
 
-      <div className="bg-white p-6 rounded-lg shadow-lg">
+      <div className="bg-white dark:bg-neutral-700 p-6 rounded-lg shadow-lg">
         <table className="table-auto w-full text-left border">
           <thead>
-            <tr className="bg-gray-100">
-              <th className="px-6 py-3 text-gray-800 font-medium">Name</th>
+            <tr className="bg-gray-100 dark:bg-neutral-600">
+              <th className="px-6 py-3 text-gray-800 dark:text-gray-100 font-medium">Name</th>
               <th
-                className="px-4 py-3 text-gray-800 font-medium text-center border-l-2"
+                className="px-4 py-3 text-gray-800 dark:text-gray-100 font-medium text-center border-l-2 dark:border-neutral-300"
                 style={{ width: "80px" }}
               >
                 Action
               </th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="dark:border-neutral-300">
             {categories.map((category) => (
               <tr
                 key={category.id}
-                className="hover:bg-gray-50 transition duration-200"
+                className="hover:bg-gray-50 dark:hover:bg-neutral-600 transition duration-200"
               >
-                <td className="border px-6 py-4">{category.name}</td>
+                <td className="border px-6 py-4 dark:border-neutral-500 dark:text-gray-300">{category.name}</td>
                 <td
-                  className="border px-2 py-4 flex justify-center items-center gap-6"
+                  className="border px-2 py-4 flex justify-center items-center gap-6 dark:border-neutral-500"
                   style={{ width: "80px" }}
                 >
                   <button
                     aria-label="Edit"
-                    className="text-blue-500 hover:text-blue-700"
+                    className="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-600"
                   >
                     <MdModeEdit fontSize={20} />
                   </button>
                   <button
                     aria-label="Delete"
-                    className="text-red-500 hover:text-red-700 ml-2"
+                    className="text-red-500 hover:text-red-700 ml-2 dark:text-red-400 dark:hover:text-red-600"
+                    onClick={() => isDeleteModalOpen(true)}
                   >
                     <AiFillDelete fontSize={20} />
                   </button>
@@ -106,30 +108,30 @@ export default function Categories() {
         </table>
       </div>
 
-      {isModalOpen && (
+      {isAddModalOpen && (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center transition-opacity duration-300">
-          <div className="bg-white p-6 rounded-lg shadow-lg w-1/3 relative">
+          <div className="bg-white dark:bg-neutral-700 p-6 rounded-lg shadow-lg w-1/3 relative">
             <button
-              className="absolute top-3 right-3 text-gray-400 hover:text-gray-600"
-              onClick={() => setIsModalOpen(false)}
+              className="absolute top-3 right-3 text-gray-400 hover:text-gray-600 dark:text-gray-300 dark:hover:text-gray-400"
+              onClick={() => setIsAddModalOpen(false)}
             >
               <FiX fontSize={24} />
             </button>
-            <h2 className="text-2xl font-semibold mb-4">Add Category</h2>
+            <h2 className="text-2xl font-semibold mb-4 dark:text-white">Add Category</h2>
             <input
               type="text"
               value={newCategoryName}
               onChange={(e) => setNewCategoryName(e.target.value)}
               placeholder="Category Name"
-              className="w-full px-4 py-2 mb-0.5 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-300"
+              className="w-full px-4 py-2 mb-0.5 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-300 dark:bg-neutral-600 dark:border-neutral-500 dark:text-gray-200"
             />
             {error && (
-              <div className="flex items-center gap-2 text-red-800 px-1 rounded-lg text-sm"><BiCommentError /> {error}</div>
+              <div className="flex items-center gap-2 text-red-800 dark:text-red-500 px-1 rounded-lg text-sm"><BiCommentError /> {error}</div>
             )}
             <div className="flex justify-end mt-4">
               <button
                 className="bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded-lg mr-2"
-                onClick={() => setIsModalOpen(false)}
+                onClick={() => setIsAddModalOpen(false)}
               >
                 Cancel
               </button>
@@ -140,6 +142,13 @@ export default function Categories() {
                 Add
               </button>
             </div>
+          </div>
+        </div>
+      )}
+      {isDeleteModalOpen && (
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center transition-opacity duration-300">
+          <div>
+            console.log("ppppppppppp")
           </div>
         </div>
       )}
