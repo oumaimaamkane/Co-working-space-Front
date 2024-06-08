413a54cc2e33c062b1d874a525abc7308e90baa6
oumaimaamkane <omaymaamkane@gmail.com>
2024-06-08 14:13:15 +0100

    delete the dashboard file

--- src/views/BackOffice/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function dashboard() {
-  return (
-    <div className="bg-white h-auto px-10 mx-5 flex justify-between items-center rounded-lg shadow-lg">
-      <h1>dashboard</h1>
-    </div>  )
-}
